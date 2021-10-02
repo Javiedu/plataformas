687ce212d11c6e16124ed9f6eeb6a2a09f0e1fe4
javiedu <jafezca.edu@gmail.com>
2021-10-02 23:08:35 +0200

    Arreglo en las plataformas.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 var suelo;
+var tiempo = 0;
 
 var mainState={
 
 preload:function(){
     game.load.image('fondo', 'assets/fondo1.png')
-    game.load.image('jugador', 'assets/personaje.png')
+    game.load.image('jugador', 'assets/40x40b.png')
     game.load.image('meta', 'assets/meta.png')
     game.load.image('plat_h2b', 'assets/80x40b.png')
     game.load.image('plat_h3b', 'assets/120x40b.png')
@@ -12,12 +13,13 @@ preload:function(){
     game.load.image('plat_h6b', 'assets/240x40b.png')
     game.load.image('plat_v2b', 'assets/40x80b.png')
     game.load.image('plat_v3b', 'assets/40x120b.png')
-    game.load.image('plat_v8b', 'assets/40x320b.png')
+    game.load.image('plat_v7b', 'assets/40x280b.png')
     game.load.image('plat_h2r', 'assets/80x40r.png')
     game.load.image('plat_h3r', 'assets/120x40r.png')
     game.load.image('plat_v4r', 'assets/40x160r.png')
     game.load.image('plat_h2t', 'assets/80x40t.png')
-    game.load.image('plat_h3t', 'assets/120x40t.png')
+    game.load.image('plat_c', 'assets/40x40b.png')
+    //game.load.image('plat_h3t', 'assets/120x40t.png')
 },	
 	
 create:function(){
@@ -37,29 +39,30 @@ create:function(){
     this.plat_hor.create(720, 520, 'plat_h2b');
     this.plat_hor.create(880, 440, 'plat_h6b');
     this.plat_hor.create(680, 160, 'plat_h6b');
-    this.plat_hor.create(320, 320, 'plat_h3b');
+    this.plat_hor.create(370, 320, 'plat_h3b');
+    this.plat_hor.create(1120, 160, 'plat_c');
 
     this.plat_ver.create(560, 560, 'plat_v3b');
     this.plat_ver.create(880, 480, 'plat_v2b');
-    this.plat_ver.create(1120, 160, 'plat_v8b');
+    this.plat_ver.create(1120, 200, 'plat_v7b');
 
     this.plat_roj.create(640, 520, 'plat_h2r');
     this.plat_roj.create(800, 520, 'plat_h2r');
-    this.plat_roj.create(440, 320, 'plat_h3r');
-    this.plat_roj.create(200, 320, 'plat_h3r');
+    this.plat_roj.create(490, 320, 'plat_h3r');
+    this.plat_roj.create(250, 320, 'plat_h3r');
     this.plat_roj.create(880, 200, 'plat_v4r');
 
     this.plat_temp.create(1040, 360, 'plat_h2t')
-    this.plat_temp.create(920, 280, 'plat_h2t')
-    this.plat_temp.create(1000, 160, 'plat_h3t')
+    this.plat_temp.create(920, 240, 'plat_h2t')
+    //this.plat_temp.create(1000, 160, 'plat_h3t')
 
-    this.meta = game.add.sprite(360, 280, 'meta');
-    this.jugador = game.add.sprite(300, 500, 'jugador')
+    this.meta = game.add.sprite(410, 280, 'meta');
+    this.jugador = game.add.sprite(240, 600, 'jugador')
 
     /* Fisicas Jugador */
     this.jugador.enableBody = true;
     this.game.physics.arcade.enable(this.jugador)
-    this.jugador.body.collideWorldBounds = true;
+    this.jugador.body.collideWorldBounds = false;
     this.jugador.body.gravity.y = 2000;
 
     this.game.physics.arcade.enable(this.plat_hor)
@@ -81,12 +84,14 @@ create:function(){
 },
 
 update:function(){
-    
-    suelo = false
+    tiempo += 1;
+    console.log(tiempo);
+    suelo = false;
 
     this.game.physics.arcade.collide(this.jugador, this.plat_roj, function(jugador, plat_roj){
-        jugador.body.x = 300
-        jugador.body.y = 500
+        jugador.body.x = 240
+        jugador.body.y = 600
+        tiempo = 0;
     })
     this.game.physics.arcade.collide(this.jugador, this.plat_hor, function(jugador, plat_hor) {
         suelo = true
