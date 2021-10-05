519e7dd57dd1aad20d3ea6ceca1cdd2457be9f12
javiedu <jafezca.edu@gmail.com>
2021-10-05 21:00:16 +0200

    Añadido nuevo mapa en producción.

--- js/main2.js
+++ js/main2.js
@@ -18,7 +18,6 @@ preload:function(){
     game.load.image('barrera_h', 'assets/barrera_h.png')
     game.load.image('plat_h2b', 'assets/80x40b.png')
     game.load.image('plat_h3b', 'assets/120x40b.png')
-    game.load.image('plat_h4b', 'assets/160x40b.png')
     game.load.image('plat_h5b', 'assets/200x40b.png')
     game.load.image('plat_h6b', 'assets/240x40b.png')
     game.load.image('plat_v2b', 'assets/40x80b.png')
@@ -26,11 +25,8 @@ preload:function(){
     game.load.image('plat_v7b', 'assets/40x280b.png')
     game.load.image('plat_h2r', 'assets/80x40r.png')
     game.load.image('plat_h3r', 'assets/120x40r.png')
-    game.load.image('plat_h4r', 'assets/160x40r.png')
     game.load.image('plat_v4r', 'assets/40x160r.png')
-    game.load.image('plat_v13r', 'assets/40x520r.png')
     game.load.image('plat_h2t', 'assets/80x40t.png')
-    game.load.image('plat_ct', 'assets/40x40t.png')
     game.load.image('plat_c', 'assets/40x40b.png')
     //game.load.image('plat_h3t', 'assets/120x40t.png')
 },	
@@ -50,57 +46,48 @@ create:function(){
     this.bordes = game.add.group()
 
 
-    this.plat_hor.create(960, 640, 'plat_h5b')
-    this.plat_hor.create(880, 40, 'plat_h3b')
-    this.plat_hor.create(760, 160, 'plat_h5b')
-    this.plat_hor.create(640, 40, 'plat_h4b')
-    this.plat_hor.create(520, 160, 'plat_h4b')
-    this.plat_hor.create(520, 40, 'plat_c')
+    this.plat_hor.create(240, 640, 'plat_h5b');
+    this.plat_hor.create(560, 520, 'plat_h2b');
+    this.plat_hor.create(720, 520, 'plat_h2b');
+    this.plat_hor.create(880, 440, 'plat_h6b');
+    this.plat_hor.create(680, 160, 'plat_h6b');
+    this.plat_hor.create(370, 320, 'plat_h3b');
+    //this.plat_hor.create(1120, 160, 'plat_c');
 
-    this.plat_ver.create(1160, 40, 'plat_v3b')
+    this.plat_ver.create(560, 560, 'plat_v3b');
+    this.plat_ver.create(880, 480, 'plat_v2b');
+    this.plat_ver.create(1120, 200, 'plat_v7b');
 
-    this.plat_roj.create(920, 200, 'plat_v13r')
-    this.plat_roj.create(1160, 200, 'plat_v13r')
-    this.plat_roj.create(1000, 40, 'plat_h4r')
-    this.plat_roj.create(800, 40, 'plat_h2r')
-    this.plat_roj.create(680, 160, 'plat_h2r')
-    this.plat_roj.create(560, 40, 'plat_h2r')
+    this.plat_roj.create(640, 520, 'plat_h2r');
+    this.plat_roj.create(800, 520, 'plat_h2r');
+    this.plat_roj.create(490, 320, 'plat_h3r');
+    this.plat_roj.create(250, 320, 'plat_h3r');
+    this.plat_roj.create(880, 200, 'plat_v4r');
+    this.plat_roj.create(1080, 160, 'plat_h2r');
 
-    this.plat_temp.create(960, 520, 'plat_h2t')
-    this.plat_temp.create(1120, 520, 'plat_ct')
-    this.plat_temp.create(1080, 400, 'plat_h2t')
-    this.plat_temp.create(960, 400, 'plat_ct')
-    this.plat_temp.create(960, 280, 'plat_h2t')
-    this.plat_temp.create(1120, 280, 'plat_ct')
-    this.plat_temp.create(1080, 160, 'plat_h2t')
-    this.plat_temp.create(960, 160, 'plat_ct')
+    this.plat_temp.create(1040, 360, 'plat_h2t')
+    this.plat_temp.create(920, 240, 'plat_h2t')
 
-    /*
-    this.barreras.create(960, 80, 'barrera_h')
-    this.barreras.create(920, 80, 'barrera_h')
-    this.barreras.create(880, 80, 'barrera_h')
-    */
+    this.barreras.create(920, 160, 'barrera_v')
+    //this.barreras.create(1115, 160, 'barrera_v')
+    //this.barreras.create(1115, 200, 'barrera_v')
+    this.barreras.create(875, 440, 'barrera_v')
+    this.barreras.create(555, 520, 'barrera_v')
+    this.barreras.create(440, 640, 'barrera_v')
+    this.barreras.create(235, 640, 'barrera_v')
+    
 
-    this.barreras_temp.create(960, 560, 'barrera_h')
-    this.barreras_temp.create(1000, 560, 'barrera_h')
-    this.barreras_temp.create(1120, 560, 'barrera_h')
-    this.barreras_temp.create(960, 440, 'barrera_h')
-    this.barreras_temp.create(1080, 440, 'barrera_h')
-    this.barreras_temp.create(1120, 440, 'barrera_h')
-    this.barreras_temp.create(960, 320, 'barrera_h')
-    this.barreras_temp.create(1000, 320, 'barrera_h')
-    this.barreras_temp.create(1120, 320, 'barrera_h')
-    this.barreras_temp.create(960, 200, 'barrera_h')
-    this.barreras_temp.create(1080, 200, 'barrera_h')
-    this.barreras_temp.create(1120, 200, 'barrera_h')
-    this.barreras_temp.create(1040, 520, 'barrera_v')
-    this.barreras_temp.create(1115, 520, 'barrera_v')
-    this.barreras_temp.create(1000, 400, 'barrera_v')
-    this.barreras_temp.create(1075, 400, 'barrera_v')
-    this.barreras_temp.create(1040, 280, 'barrera_v')
-    this.barreras_temp.create(1115, 280, 'barrera_v')
-    this.barreras_temp.create(1000, 160, 'barrera_v')
-    this.barreras_temp.create(1075, 160, 'barrera_v')
+    this.barreras.create(450, 360, 'barrera_h')
+    this.barreras.create(410, 360, 'barrera_h')
+    this.barreras.create(370, 360, 'barrera_h')
+
+    this.barreras_temp.create(1000, 240, 'barrera_v')
+    this.barreras_temp.create(1035, 360, 'barrera_v')
+
+    this.barreras_temp.create(1040, 400, 'barrera_h')
+    this.barreras_temp.create(1080, 400, 'barrera_h')
+    this.barreras_temp.create(920, 280, 'barrera_h')
+    this.barreras_temp.create(960, 280, 'barrera_h')
 
     this.bordes.create(0, 0, 'b_lateral')
     this.bordes.create(1280, 0, 'b_lateral')
@@ -109,7 +96,7 @@ create:function(){
     //this.plat_temp.create(1000, 160, 'plat_h3t')
 
     this.meta = game.add.sprite(410, 280, 'meta');
-    this.jugador = game.add.sprite(960, 600, 'jugador')
+    this.jugador = game.add.sprite(240, 600, 'jugador')
 
     /* Fisicas Jugador */
     this.jugador.enableBody = true;
@@ -136,12 +123,12 @@ create:function(){
     this.game.physics.arcade.enable(this.barreras)
     this.barreras.setAll('enableBody', true)
     this.barreras.setAll('body.immovable', true)
-    //this.barreras.setAll('alpha', 0);
+    this.barreras.setAll('alpha', 0);
 
     this.game.physics.arcade.enable(this.barreras_temp)
     this.barreras_temp.setAll('enableBody', true)
     this.barreras_temp.setAll('body.immovable', true)
-    //this.barreras_temp.setAll('alpha', 0);
+    this.barreras_temp.setAll('alpha', 0);
 
     this.game.physics.arcade.enable(this.bordes)
     this.bordes.setAll('enableBody', true)
@@ -158,15 +145,15 @@ update:function(){
     t_barrera = false;
     tiempo += 1;
     if(tiempo == 76){ tiempo = 0 }
-    /*
+
     if( tiempo == 0 ){ temp = true }
     if( tiempo == cambio && temp == true ){ 
         temp = false;
         cambio = Math.floor((Math.random() * (40 - 25)) + 25);
     }
-    */
+
     this.game.physics.arcade.collide(this.jugador, this.plat_roj, function(jugador, plat_roj){
-        jugador.body.x = 960
+        jugador.body.x = 240
         jugador.body.y = 600
         jugador.body.velocity.x = 0
         jugador.body.velocity.y = 0
@@ -230,7 +217,6 @@ update:function(){
         this.jugador.body.velocity.x = 400;
     } else if(this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && suelo == true && t_barrera == false){
         this.jugador.body.velocity.y = -800;
-        this.jugador.body.velocity.x = 0;
     } else if(suelo == true){
         this.jugador.body.velocity.x = 0;
     }
